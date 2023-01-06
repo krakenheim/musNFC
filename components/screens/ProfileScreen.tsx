feec07df3917eb216e5de67fa946086c9ec13c7c
krakenheim <j.n.krag@gmail.com>
2023-01-06 17:58:30 +0100

    profile picture and name

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+//! Inspired by https://www.youtube.com/watch?v=Zw7FkwXotxg
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Platform,
+  ScrollView,
+} from "react-native";
 import React from "react";
 
 export default function ProfileScreen(navigation) {
   return (
-    <View style={styles.container}>
-      <Text style={{fontWeight: "bold"}}>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={{ alignSelf: "center", marginTop: 100 }}>
+        <View style={styles.profileImage}>
+          <Image
+            source={require("../../assets/images/profilePic.jpg")}
+            style={styles.image}
+            resizeMode="center"
+          ></Image>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.profileText}>Jacob Krag</Text>
+        </View>
+
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +37,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? 40 : 0,
+  },
+  image: {
+    flex: 1,
+    height: undefined,
+    width: undefined,
+    resizeMode: "cover",
+  },
+  profileImage: {
+    height: 180,
+    aspectRatio: 1,
+    borderRadius: 90,
+    overflow: "hidden",
+  },
+  infoContainer: {
+    alignSelf: "center",
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: 16,
+  },
+  profileText: {
+    fontWeight: "200",
+    fontSize: 36,
   },
 });
